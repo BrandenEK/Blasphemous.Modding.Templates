dd8220f05c5f0072fd174540acbb979c4b455b04
Branden Wood <brandenek03912@hotmail.com>
2023-09-12 22:23:23 -0400

    Update dependency style for blas2 mods

--- content/BlasII.ModTemplate/Properties/AssemblyInfo.cs
+++ content/BlasII.ModTemplate/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.ModTemplate;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.ModTemplate.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonAdditionalDependencies("ModdingAPI")]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
