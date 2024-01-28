50c1a58cafb1f61e9df2facd0a2f99a63dccbfac
unknown <brandenek03912@hotmail.com>
2024-01-27 21:07:33 -0500

    blas2: strip project file

--- content/BlasII.ModTemplate/Main.cs
+++ content/BlasII.ModTemplate/Main.cs
@@ -1,14 +1,13 @@
 ﻿using MelonLoader;
 
-namespace BlasII.ModTemplate
-{
-    public class Main : MelonMod
-    {
-        public static ModTemplate ModTemplate { get; private set; }
+namespace BlasII.ModTemplate;
 
-        public override void OnLateInitializeMelon()
-        {
-            ModTemplate = new ModTemplate();
-        }
+public class Main : MelonMod
+{
+    public static ModTemplate ModTemplate { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        ModTemplate = new ModTemplate();
     }
 }
--- content/BlasII.ModTemplate/ModTemplate.cs
+++ content/BlasII.ModTemplate/ModTemplate.cs
@@ -1,14 +1,13 @@
 ﻿using BlasII.ModdingAPI;
 
-namespace BlasII.ModTemplate
-{
-    public class ModTemplate : BlasIIMod
-    {
-        public ModTemplate() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+namespace BlasII.ModTemplate;
 
-        protected override void OnInitialize()
-        {
-            LogError($"{ModInfo.MOD_NAME} is initialized");
-        }
+public class ModTemplate : BlasIIMod
+{
+    public ModTemplate() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} is initialized");
     }
 }
