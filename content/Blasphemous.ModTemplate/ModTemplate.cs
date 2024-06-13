05c3aebf46a77ba4c6a235b7ef62aeb7c9d4eb79
unknown <brandenek03912@hotmail.com>
2024-06-13 14:11:46 -0400

    blas1: keep initialize method

--- content/Blasphemous.ModTemplate/ModTemplate.cs
+++ content/Blasphemous.ModTemplate/ModTemplate.cs
@@ -5,4 +5,9 @@ namespace Blasphemous.ModTemplate;
 public class ModTemplate : BlasMod
 {
     internal ModTemplate() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
 }
